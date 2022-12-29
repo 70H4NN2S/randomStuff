dfca4a8048745763eebe005f477e4b6be167960e
Johannes Niedens <johannesniedens@gmail.com>
2022-12-29 22:19:13 +0100

    Mastermind play functionality added

--- Mastermind/Mastermind.java
+++ Mastermind/Mastermind.java
@@ -1,10 +1,45 @@
 import java.util.*;
 
 public class Mastermind {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		if(args.length == 0)
 			playMastermind();
 		else
 			System.out.println("Start the game without arguments.");
+	}
+
+	public static void playMastermind()
+	{
+		String 	result 	= getNewRandomNumber();
+		String 	input 	= "";
+		Scanner scanner = new Scanner(System.in);
+		boolean hasWon 	= false;
+		for (int i = 0; i < 10; i++)
+		{
+			System.out.print("Introduce a number.\n==>");
+			input = scanner.next();
+			if (isWrongInput(input))
+			{
+				System.out.println("ERROR: Please introduce only four numbers between 1 and 8. Try again.\n");
+				i--;
+			}
+			else if (result.equals(input))
+			{
+				hasWon = true;
+				break;
+			}
+			else
+			{
+				giveFeedbackToPlayer(input, result);
+			}
+		}
+		if (hasWon)
+			System.out.println("You have won the game.");
+		else
+		{
+			System.out.println("You have sadly lost. Good luck next time. The result is: " + result);
+		}
+		scanner.close();
 	}
 }
