9104801d0c2f3e6babfccfecfbcfefb713492304
Johannes Niedens <johannesniedens@gmail.com>
2022-12-29 22:12:04 +0100

    Mastermind main added

---
+++ Mastermind/Mastermind.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Mastermind {
+	public static void main(String[] args) {
+		if(args.length == 0)
+			playMastermind();
+		else
+			System.out.println("Start the game without arguments.");
+	}
+}
